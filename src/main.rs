7cad8457c5b83ba9838e209d1a500c5e7d92ede6
Ilya Maximov <mail@overlisted.net>
2020-12-21 12:54:49 +0100

    add a command for scoring

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ use crate::system::Leaderboard;
 use dashmap::DashMap;
 use serenity::client::{Context, EventHandler};
 use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::CommandResult;
+use serenity::framework::standard::{Args, CommandResult};
 use serenity::framework::StandardFramework;
 use serenity::model::channel::Message;
 use serenity::model::id::GuildId;
 use serenity::{async_trait, Client};
+use std::sync::Arc;
 
 mod system;
+mod view;
 
 #[group]
+#[commands(score)]
 struct General;
 
 struct Handler;
@@ -41,6 +44,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     client.start().await?;
+
+    Ok(())
+}
+
+#[command]
+async fn score(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let winner = args.single_quoted::<String>()?;
+    let loser = args.single_quoted::<String>()?;
+
+    let mut data = ctx.data.write().await;
+    let lbs = data
+        .get_mut::<Leaderboard>()
+        .expect("There's no leaderboard storage!");
+    let mut lb = match lbs.get_mut(&msg.guild_id.expect("Guild ID is None!")) {
+        Some(lbs) => lbs,
+        None => {
+            msg.reply(ctx, "This guild doesn't have a leaderboard!")
+                .await?;
+            return Ok(());
+        }
+    };
+
+    let (winner, loser) = lb.score(winner, loser);
+    msg.reply(
+        ctx,
+        format!(
+            "**Ok**!\n{}\n{}",
+            lb.format_player(&winner),
+            lb.format_player(&loser)
+        ),
+    )
+    .await?;
 
     Ok(())
 }
